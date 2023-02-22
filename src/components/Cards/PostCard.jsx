926a6ef896eafc7a06cb221c3cb3687abffc141b
Sauvik Nath <sauviknath10@gmail.com>
2023-02-22 14:49:35 +0530

    Changes for #1 - Design Landing Page

--- src/components/Cards/PostCard.jsx
+++ src/components/Cards/PostCard.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import CategoryCard from './CategoryCard';
 
 function PostCard() {
     const [isLoading, setIsLoading] = useState(true);
@@ -39,11 +40,10 @@ function PostCard() {
     }, []);
 
     return (
-        <div className="mt-10">
+        <div className="">
             <div className="relative w-700">
-
+                <CategoryCard/>
                 {isLoading ? (
-
                     <div className="m-8 relative space-y-6">
                         <div className="p-5 bg-white rounded-lg flex items-center justify-between space-x-8">
                             <div className="flex-1">
@@ -74,11 +74,9 @@ function PostCard() {
                     <div>
                         {posts.map(post => (
                             <div key={post.id} className="relative">
-                                <div className=" p-5 bg-white rounded-lg lg:ml-7 mr-10 mt-6 ml-4 space-y-3">
+                                <div className="transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg p-5 bg-white rounded-lg lg:ml-7 mr-10 mb-6 mt-6 lg:mt-0 ml-4 space-y-3">
                                     <h3 className="text-sm font-bold text-gray-700">@user{post.id}</h3>
                                     <h3 className="text-lg font-bold text-gray-700">{post.title}</h3>
-                                    
-                                   
                                     <div className="flex items-center justify-between mt-4">
                                         <div className="flex items-center space-x-4">
                                             <div className="flex items-center ">
@@ -96,23 +94,21 @@ function PostCard() {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="rounded-r-lg absolute top-0 right-0 flex flex-col bg-gradient-to-b from-blue-200 to-blue-300 h-full w-10 md:mr-0 lg:mr-0 mr-5">
-                                    <button className="text-gray-500 mt-2" onClick={() => handleUpvote(post.id)}>
+                                <div className="rounded-r-lg absolute top-0 right-0 flex flex-col bg-gradient-to-b from-indigo-800 via-indigo-500 to-indigo-600 h-full w-10 md:mr-0 lg:mr-0 mr-5">
+                                    <button className="text-gray-100 mt-2" onClick={() => handleUpvote(post.id)}>
                                         <span role="img" aria-label="upvote">&#9650;</span>
                                     </button>
-                                    <p className="text-center">{(counts[post.id]?.upvotes || 0) - (counts[post.id]?.downvotes || 0)}</p>
-                                    <button className="text-gray-500" onClick={() => handleDownvote(post.id)}>
+                                    <p className="text-center text-gray-100">{(counts[post.id]?.upvotes || 0) - (counts[post.id]?.downvotes || 0)}</p>
+                                    <button className="text-gray-100" onClick={() => handleDownvote(post.id)}>
                                         <span role="img" aria-label="downvote">&#9660;</span>
                                     </button>
                                 </div>
-
                             </div>
-
                         ))}
                     </div>)}
-            </div>
-        </div>
-    )
-}
+               </div>
+         </div>
+       )
+   }
 
 export default PostCard
