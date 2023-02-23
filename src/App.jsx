316054088c6acde617aaabfdd392665bcbed0437
Sauvik Nath <sauviknath10@gmail.com>
2023-02-23 18:02:08 +0530

    Done #3 - Design SignIn and SignUp Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
+import React, { useState } from 'react';
 import Landing from './pages/Landing'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignIn from './pages/SignIn';
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Landing />}></Route>
-      <Route exact path="/create-profile" element={<SignIn/>}></Route>
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import { useNavigate } from "react-router-dom";
 
-function Navbar() {
-  const navigate = useNavigate();
+function Navbar({ handleOpenModal }) {
   const [isScrolled, setIsScrolled] = useState(false);
-
-  const handleNavigate = () => {
-    navigate('/create-profile');
-  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -27,10 +21,10 @@ function Navbar() {
 
   return (
     <nav className={`${isScrolled
-      ? 'bg-slate-900 bg-opacity-75 shadow-lg'
+      ? 'bg-slate-900 bg-opacity-80 shadow-lg'
       : 'bg-slate-100'
       } transition-all duration-500 ease-in-out fixed sticky top-0 z-50 shadow-lg backdrop-filter backdrop-blur-lg backdrop-saturate-150`}>
-      <div className="container mx-auto px-4 py-5 flex justify-between items-center">
+      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         <a className={`${isScrolled
           ? 'font-bold text-white text-2xl lg:text-2xl'
           : 'font-bold text-2xl lg:text-2xl'
@@ -47,14 +41,12 @@ function Navbar() {
               <div className="relative inline-flex w-3 h-3 bg-red-500 border-2 border-white rounded-full -top-2 right-3 dark:border-gray-900"></div>
             </div>
           </button>
-
           <button className="ml-3 flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-800 hover:border-violet-500 appearance-none focus:outline-none">
             <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <title>Menu</title>
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
           </button>
-
         </div>
         <div className="hidden lg:block">
           <ul className="inline-flex items-center">
@@ -69,11 +61,11 @@ function Navbar() {
                 </div>
               </button>
             </li>
-            <li><a href="" onClick={handleNavigate} className={`${isScrolled
+            <li><button onClick={() => handleOpenModal('signin')} className={`${isScrolled
               ? 'border-2 border-indigo-500 rounded-md p-2 text-gray-100 ml-4'
               : 'border-2 border-indigo-500 rounded-md p-2 text-gray-900 ml-4'
-              }`}>Sign In</a></li>
-            <li><a href="" className="bg-indigo-500 rounded-md p-2 text-gray-100 ml-6">Sign Up</a></li>
+              }`}>Sign In</button></li>
+            <li><button onClick={() => handleOpenModal('signup')} className="bg-indigo-500 rounded-md p-2 text-gray-100 ml-6">Sign Up</button></li>
           </ul>
         </div>
       </div>
