2aecbf543345f4077ac069669de628763934861b
Sauvikn98 <sauviknath10@gmail.com>
2023-06-04 17:26:06 +0530

    latest

--- src/lib/api/index.jsx
+++ src/lib/api/index.jsx
@@ -1,5 +1,5 @@
 // user api
-export const API_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = 'https://agora-space.onrender.com';
 export const API_USERS_REGISTER = `${API_BASE_URL}/users/register`;
 export const API_USERS_LOGIN = `${API_BASE_URL}/users/login`;
 export const API_USERS_BY_NAME = `${API_BASE_URL}/users/:userName`;
